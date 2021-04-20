f5048b04bfb224f538280b51f91799283954e75e
tobarbosa <thiago.pura@hotmail.com>
2021-04-20 12:01:20 -0300

    Mudança na aquisição de champions

--- scripts/champions.py
+++ scripts/champions.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import requests
+import json
 
-champ = pd.read_json('champions.json')
+champ_json = requests.get('http://ddragon.leagueoflegends.com/cdn/11.8.1/data/en_US/champion.json')
+champ = pd.read_json(path_or_buf=json.dumps(champ_json.json()))
 champId = pd.DataFrame(columns=['id', 'Champion Name', 'Champion Category'])
 for i in champ['data']:
     # print({'id':i['key'], 'Champion Name':i['id']})
